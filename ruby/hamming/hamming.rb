598d867bc74ebbea641470de95fe4b6b8ebcfeaa
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2023-01-10 23:27:23 -0700

    Refactor using each_with_index

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -7,7 +7,12 @@ To get started with TDD, see the `README.md` file in your
 =end
 class Hamming
     def self.compute(strand_1, strand_2)
-        0 if strand_1 == strand_2
+        hamming_distance = 0
+        # 0 if strand_1 == strand_2
+        strand_1.chars.each_with_index do |char, i|
+            hamming_distance += 1 unless char == strand_2[i]
+        end
+        hamming_distance
     end
 end
 
--- ruby/hamming/hamming_test.rb
+++ ruby/hamming/hamming_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require_relative 'hamming'
+require 'pry'
 
 class HammingTest < Minitest::Test
   def test_empty_strands
