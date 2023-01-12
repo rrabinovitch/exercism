3da219da9700ba3bb6db021ef1d7914781fc0790
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2023-01-12 14:43:45 -0700

    Pass tests

--- javascript/freelancer-rates/freelancer-rates.js
+++ javascript/freelancer-rates/freelancer-rates.js
@@ -26,7 +26,7 @@
  * @returns {number} the rate per day
  */
 export function dayRate(ratePerHour) {
-  throw new Error('Remove this line and implement the function');
+  return ratePerHour * 8
 }
 
 /**
@@ -37,7 +37,7 @@ export function dayRate(ratePerHour) {
  * @returns {number} the number of days
  */
 export function daysInBudget(budget, ratePerHour) {
-  throw new Error('Remove this line and implement the function');
+  return Math.floor(budget / dayRate(ratePerHour))
 }
 
 /**
@@ -49,5 +49,13 @@ export function daysInBudget(budget, ratePerHour) {
  * @returns {number} the rounded up discounted rate
  */
 export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
-  throw new Error('Remove this line and implement the function');
+  const BILLABLE_DAYS_PER_MONTH = 22
+  var numDiscountedMonths = Math.floor(numDays / BILLABLE_DAYS_PER_MONTH)
+  var numDiscountedDays = numDiscountedMonths * BILLABLE_DAYS_PER_MONTH
+  var discountedMonthsCost = (numDiscountedDays * dayRate(ratePerHour)) * (1 - discount)
+
+  var numNonDiscountedDays = numDays % BILLABLE_DAYS_PER_MONTH
+  var nonDiscountedDaysCost = numNonDiscountedDays * dayRate(ratePerHour)
+
+  return Math.ceil(nonDiscountedDaysCost + discountedMonthsCost)
 }
