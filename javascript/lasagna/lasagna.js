2ceabfc87ab8a8eb43506592cbfc717425067744
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2023-01-12 12:54:20 -0700

    Implement #totalTimeInMinuts

--- javascript/lasagna/lasagna.js
+++ javascript/lasagna/lasagna.js
@@ -70,5 +70,5 @@ export function preparationTimeInMinutes(numberOfLayers) {
  * @returns {number} the total working time
  */
 export function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
-  throw new Error('Remove this line and implement the function');
+  return (numberOfLayers * PREPARATION_MINUTES_PER_LAYER) + actualMinutesInOven
 }
