f3242121a9fc8be0b1361fd2323af6e1b483f2d4
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2023-01-17 23:39:33 -0700

    Set up basic methods

---
+++ ruby/bowling/bowling.rb
@@ -0,0 +1,16 @@
+=begin
+Write your code for the 'Bowling' exercise in this file. Make the tests in
+`bowling_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/bowling` directory.
+=end
+class Game
+    def roll(pins)
+        
+    end
+
+    def score
+        
+    end
+end
