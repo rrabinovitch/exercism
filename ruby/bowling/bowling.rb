05aa6e7e9c17e661077ca21c2eec91846eb895a2
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2023-01-19 00:01:05 -0700

    Track roll history by frame

--- ruby/bowling/bowling.rb
+++ ruby/bowling/bowling.rb
@@ -10,9 +10,27 @@ class Game
     
     def initialize
         @score = 0
+        @frames = Hash.new()
+        @current_frame = 1
     end
 
-    def roll(pins)
-        @score += pins
+    def roll(pins_hit)
+        # if the first roll of a frame hasn't been recorded yet, create a new hash key
+        if @frames[@current_frame].nil?
+            # record first roll
+            @frames[@current_frame] = [pins_hit]
+        else # if first roll of a frame has already been recoreed, record the second roll
+            @frames[@current_frame] << pins_hit
+        end
+        # if pins_hit represents the second roll of a frame OR if pins_hit == 10: @current_frame += 1
+        if @frames[@current_frame].count == 2 || pins_hit == 10
+            # "end" the current frame and set up for next frame by adding 1 to @current_frame
+            @current_frame += 1
+        end
+        pins_hit # overriding implicit return of above conditional so that relevant info is returned instead of current frame number
+    end
+
+    def score
+        binding.pry
     end
 end
--- ruby/bowling/bowling_test.rb
+++ ruby/bowling/bowling_test.rb
@@ -1,9 +1,10 @@
 require 'minitest/autorun'
 require_relative 'bowling'
+require 'pry'
 
 class BowlingTest < Minitest::Test
   def test_should_be_able_to_score_a_game_with_all_zeros
-    # skip
+    skip
     game = Game.new
     rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     rolls.each { |pins| game.roll(pins) }
@@ -11,7 +12,7 @@ class BowlingTest < Minitest::Test
   end
 
   def test_should_be_able_to_score_a_game_with_no_strikes_or_spares
-    # skip
+    skip
     game = Game.new
     rolls = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]
     rolls.each { |pins| game.roll(pins) }
@@ -19,7 +20,7 @@ class BowlingTest < Minitest::Test
   end
 
   def test_a_spare_followed_by_zeros_is_worth_ten_points
-    # skip
+    skip
     game = Game.new
     rolls = [6, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     rolls.each { |pins| game.roll(pins) }
